8748570ecc6ac52e97033c7d47c2f58de4bba439
Maksim <i@mpozdnjakov.ru>
2023-08-04 17:49:02 +0300

    commit 15

--- app/src/main/java/PozMaxPav/com/all_activities/SleepStatistics.java
+++ app/src/main/java/PozMaxPav/com/all_activities/SleepStatistics.java
@@ -1,14 +1,34 @@
 package PozMaxPav.com.all_activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import PozMaxPav.com.R;
 
 public class SleepStatistics extends AppCompatActivity {
+
+    private Button back_button_statistics;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_sleepstatistics);
+
+        addListenerOnButton();
+    }
+
+    private void addListenerOnButton() {
+
+        back_button_statistics = findViewById(R.id.back_button_statistics);
+
+        back_button_statistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
     }
 }
