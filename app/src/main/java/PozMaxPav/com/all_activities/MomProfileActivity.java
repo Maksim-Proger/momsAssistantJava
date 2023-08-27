0dfc27f7d75b343c2a36a4d06de0f5df8a2ad568
Maksim <i@mpozdnjakov.ru>
2023-08-27 21:26:21 +0300

    commit 33

--- app/src/main/java/PozMaxPav/com/all_activities/MomProfileActivity.java
+++ app/src/main/java/PozMaxPav/com/all_activities/MomProfileActivity.java
@@ -3,13 +3,16 @@ package PozMaxPav.com.all_activities;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import PozMaxPav.com.R;
+import PozMaxPav.com.model.helperClasses.SharedPreferencesUtils;
 
 public class MomProfileActivity extends AppCompatActivity {
 
     private Button back_button;
+    private EditText editName,editSurname,editPatronymic,editEmail,editPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,36 @@ public class MomProfileActivity extends AppCompatActivity {
 
     private void addListenerOnButton() {
         back_button = findViewById(R.id.back_button);
+        // Находим наши Edit поля
+        editName = findViewById(R.id.editName);
+        String name = SharedPreferencesUtils.getKeyName(this);
+        if (name != null) {
+            editName.setText(name);
+        }
+
+        editSurname = findViewById(R.id.editSurname);
+        String surname = SharedPreferencesUtils.getKeySurname(this);
+        if (surname != null) {
+            editSurname.setText(surname);
+        }
+
+        editPatronymic = findViewById(R.id.editPatronymic);
+        String patronymic = SharedPreferencesUtils.getKeyPatronymic(this);
+        if (patronymic != null) {
+            editPatronymic.setText(patronymic);
+        }
+
+        editEmail = findViewById(R.id.editEmail);
+        String email = SharedPreferencesUtils.getKeyEmail(this);
+        if (email != null) {
+            editEmail.setText(email);
+        }
+
+        editPassword = findViewById(R.id.editPassword);
+        String password = SharedPreferencesUtils.getKeyPassword(this);
+        if (password != null) {
+            editPassword.setText(password);
+        }
 
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
