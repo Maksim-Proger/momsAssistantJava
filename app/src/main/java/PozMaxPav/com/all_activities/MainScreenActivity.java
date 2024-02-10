a4d70e9d3172c89652780839186f74060f064886
MaksimPozdniakov <i@mpozdnjakov.ru>
2024-02-11 00:02:36 +0300

    62 commits

--- app/src/main/java/PozMaxPav/com/all_activities/MainScreenActivity.java
+++ app/src/main/java/PozMaxPav/com/all_activities/MainScreenActivity.java
@@ -88,7 +88,7 @@ public class MainScreenActivity extends AppCompatActivity {
         fieldName = findViewById(R.id.fieldName);
         String name = SharedPreferencesUtils.getKeyName(this);
         if (name != null) {
-            String welcome = "Привет " + name;
+            String welcome = "Привет, " + name;
             fieldName.setText(welcome);
         }
         // endregion
